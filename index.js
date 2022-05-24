e5e430091131eae09ff72ae7fc66d23ad7950ab2
arindra97 <ar28297@gmail.com>
2022-05-24 23:15:53 +0700

    update file index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("halo, namaku adalah Muhammad Arindra Khoiru Syawal");
+const saya = {
+    'nama'      : 'Muhammad Arindra Khoiru Syawal',
+    'kelas'     : 'Rea2a',
+    'lembaga'   : 'DTS Kominfo'
+}
+
+console.log(`halo, namaku adalah ${saya.nama} \naku kelas ${saya.kelas}, aku mengikuti pelatihan react developer yang diadakan oleh ${saya.lembaga}`);
